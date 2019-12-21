a0b730eee71eb0439c4436cfe747e654655f7d69
Glitch (speckled-plier) <none>
2019-12-21 09:37:30 +0000

    ⛰📷 Checkpoint ./events/ready.js:9618758/630

--- events/ready.js
+++ events/ready.js
@@ -6,17 +6,16 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-var oyun = [
-        "w!yardım - Otorol Güncellendi🔥",
-        "w!yardım - Küfür Engel✨",
-        "w!yardım - Reklam Engel⭐️"  
+var asd = [
+        "w!yardım - Aktif Hizmet ",
+        "w!yardım - İlgili Yönetim ",
+        "w!yardım - Şehir Sistemi "  
     ];
 
     setInterval(function() {
 
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "https://www.twitch.tv/emirhansaracyt" );
+        client.user.setActivity("Akabe RP");
         }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
